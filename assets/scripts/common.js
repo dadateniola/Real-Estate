dcbfff56f38f32c5ed19b0f7323bfe8faac2d215
Dada Teniola <42898812+emmy13@users.noreply.github.com>
2024-01-16 17:36:05 +0100

    massive changes

--- assets/scripts/common.js
+++ assets/scripts/common.js
@@ -7,28 +7,140 @@ const create = (e) => document.createElement(e);
 const root = (e) => getComputedStyle(select(":root")).getPropertyValue(e);
 const getStyle = (e, style) => window.getComputedStyle(e)[style];
 
-const preventDefault = (event) => event.preventDefault();
-const disableLinksAndBtns = (condition = false) => {
-    selectAll('a, button').forEach((element) => {
-        if (condition) {
-            element.setAttribute('disabled', 'true');
-
-            if (element.tagName === 'A') {
-                element.dataset.href = element.href;
-                element.addEventListener('click', preventDefault);
-            }
-        } else {
-            selectAll('a, button').forEach((element) => {
-                element.removeAttribute('disabled');
-
-                if (element.tagName === 'A') {
-                    element.setAttribute('href', element.dataset.href);
-                    element.removeEventListener('click', preventDefault);
-                }
-            });
-        }
-    });
-}
 
 //Register Scrolltrigger
 gsap.registerPlugin(ScrollTrigger);
+
+
+var active = true;
+//Global methods
+class Methods {
+    constructor(params = {}) {
+        Object.assign(this, params);
+
+        return this;
+    }
+
+    //Statics
+    static insertToDOM(params = {}) {
+        const { type, text, parent, before, classes } = params;
+
+        if (!type || !parent) return null;
+
+        //Insert element contents
+        const element = create(type);
+
+        if (text) {
+            if (type == "img") element.src = text;
+            else element.innerHTML = text;
+        }
+
+        //Add classes
+        if (classes?.length) {
+            if (Array.isArray(classes)) classes.forEach(c => element.classList.add(c));
+            else element.classList.add(classes);
+        }
+
+        //Append element to parent
+        if (before) parent.insertBefore(element, before);
+        else parent.appendChild(element);
+    }
+
+    static checkDeviceType() {
+        const mobileThreshold = 768;
+        const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+        const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+        if (isTouchDevice && screenWidth <= mobileThreshold) {
+            return "mobile";
+        } else {
+            return "pc";
+        }
+    }
+
+    static preventDefault = (event) => event.preventDefault();
+    static disableLinksAndBtns(condition = false) {
+        selectAll('a, button').forEach((element) => {
+            if (condition) {
+                element.setAttribute('disabled', 'true');
+
+                if (element.tagName === 'A') {
+                    element.dataset.href = element.href;
+                    element.addEventListener('click', Methods.preventDefault);
+                }
+            } else {
+                selectAll('a, button').forEach((element) => {
+                    element.removeAttribute('disabled');
+
+                    if (element.tagName === 'A') {
+                        element.setAttribute('href', element.dataset.href);
+                        element.removeEventListener('click', Methods.preventDefault);
+                    }
+                });
+            }
+        });
+    }
+
+
+    //Slider
+    initializeSlider() {
+        if (!this?.holder) return null;
+
+        this.pagination();
+        this.startSlider();
+    }
+
+    startSlider() {
+        //Parameters: { holder: (parent element), max: (maximum number of images) }
+
+        const holder = this?.holder;
+        const image = selectWith(holder, "img.in-view");
+        const outImage = selectWith(holder, "img:not(.in-view)");
+
+        const max = (this?.max - 1) || 1;
+        let count = parseInt(holder.dataset?.count) || 2;
+        
+        const paginations = selectAll(".pagination-dot");
+        const currentPagination = (count - 2 <= -1) ? max : count - 2;
+        const nextPagination = (currentPagination + 1 > max) ? 0 : currentPagination + 1;
+        const laterPagination = (nextPagination + 1 > max) ? 0 : nextPagination + 1;
+        
+        const tl = gsap.timeline();
+
+        tl
+            .call(() => {
+                paginations.forEach(e => e.classList.remove("active", "next", "later"))
+                paginations.forEach((e, i) => {
+                    const type = (currentPagination == i) ? 'active' : ((nextPagination == i) ? 'next' : ((laterPagination == i) ? 'later' : null));
+                    if (type) paginations[i].classList.add(type)
+                })
+            })
+            .to(image, { opacity: 1, ease: "none" })
+            .to(image, { xPercent: -25, duration: 6.5, ease: "none" }, "<")
+            .call(() => {
+                outImage?.remove();
+                const methods = new Methods({ holder: select(".form-img"), max: max + 1 });
+                methods.startSlider();
+            }, null, "-=0.5")
+
+        Methods.insertToDOM({ type: "img", text: `/images/sign/test (${count}).jpg`, parent: holder, classes: ["hidden", "in-view"] });
+        image.classList.remove("in-view");
+
+        const newImage = selectWith(holder, "img:last-child");
+
+        gsap.set(newImage, { opacity: 0 })
+
+        newImage.classList.remove("hidden");
+
+        count = (count % (max + 1) == 0) ? 1 : count + 1;
+        holder.dataset.count = count;
+    }
+
+    pagination() {
+        Methods.insertToDOM({ type: "div", parent: this.holder, classes: "pagination" })
+
+        for (let i = 0; i < this.max; i++) {
+            Methods.insertToDOM({ type: "div", parent: select(".pagination"), classes: "pagination-dot" })
+        }
+    }
+}
---
+++ assets/scripts/sign.js
@@ -0,0 +1,179 @@
+class Animations {
+    constructor(params = {}) {
+        Object.assign(this, params);
+        this.init();
+    }
+
+    parameters() {
+        this.signPages = {
+            signUp: {
+                form: `
+                    <div>
+                        <div class="slide-in">
+                            <h1>Building your future home,</h1>
+                        </div>
+                        <div class="slide-in">
+                            <h1>One sign-up at a time</h1>
+                        </div>
+                    </div>
+                    <div class="slide-in">
+                        <input type="text" name="fullname" id="fullname" placeholder="Enter your fullname">
+                    </div>
+                    <div class="slide-in">
+                        <input type="text" name="email" id="email" placeholder="Enter your email">
+                    </div>
+                    <div class="slide-in">
+                        <input type="text" name="pass" id="pass" placeholder="Enter your password">
+                    </div>
+                    <div class="form-actions flex slide-in">
+                        <button class="arrow" href="/">
+                            <div class="sign-arrow flex center">
+                                <i class="fa-solid fa-arrow-right"></i>
+                            </div>
+                            <div class="sign-text">
+                                sign in
+                            </div>
+                        </button>
+                        <a class="wide" href="/">sign up</a>
+                    </div>
+                `,
+                quote: "Home awaits. Start your journey now."
+            },
+            signIn: {
+                form: `
+                    <div>
+                        <div class="slide-in">
+                            <h1>Unlocking the</h1>
+                        </div>
+                        <div class="slide-in">
+                            <h1>doors to your dream home</h1>
+                        </div>
+                    </div>
+                    <div class="slide-in">
+                        <input type="text" name="email" id="email" placeholder="Enter your email">
+                    </div>
+                    <div class="slide-in">
+                        <input type="text" name="pass" id="pass" placeholder="Enter your password">
+                    </div>
+                    <div class="form-actions flex slide-in">
+                    <button class="arrow" href="/">
+                        <div class="sign-arrow flex center">
+                            <i class="fa-solid fa-arrow-right"></i>
+                        </div>
+                        <div class="sign-text">
+                            sign up
+                        </div>
+                    </button>
+                        <a class="wide" href="/">sign in</a>
+                    </div>
+                `,
+                quote: "Every home tells a story: yours begins here."
+            }
+        }
+    }
+
+    init() {
+        this.parameters();
+
+        const tl = gsap.timeline();
+        const type = this?.type;
+
+        if (!type) return tl;
+
+        tl.call(() => Methods.disableLinksAndBtns(true));
+
+        if (type == 'slide') {
+            const slide = this.slide();
+            tl.add(slide);
+        };
+
+        tl.call(() => Methods.disableLinksAndBtns());
+    }
+
+    //Animations
+    slide() {
+        const slideTl = gsap.timeline();
+        const currentElements = selectAll(".slide-in > *");
+        const quote = selectAll(".quote")
+        const quoteText = select(".quote p");
+
+        const formBox = select(".form-box");
+        const type = formBox.dataset?.form;
+        const text = (type == 'signin') ? this.signPages.signUp.form : this.signPages.signIn.form;
+
+        if (!currentElements.length) return slideTl;
+
+        slideTl
+            .to(currentElements, { yPercent: -100, stagger: 0.1, duration: 0.5, opacity: 0, ease: "Back.easeIn" })
+            .to(quote, { opacity: 0 }, "<")
+
+            .call(() => {
+                select(".form-box .form").remove();
+
+                Methods.insertToDOM({ type: "div", text, parent: formBox, before: selectWith(formBox, ".quote"), classes: ["form", "flex", "hidden"] })
+                formBox.dataset.form = (type == 'signin') ? 'signup' : 'signin';
+
+                this.slideIn({ quote, quoteText, type })
+            })
+
+        return slideTl;
+    }
+
+    slideIn(params = {}) {
+        const slideInTl = gsap.timeline();
+        const { quote, quoteText, type } = params;
+        const text = (type == 'signin') ? this.signPages.signUp.quote : this.signPages.signIn.quote;
+
+        slideInTl
+            .set(".slide-in > *", { yPercent: 100, opacity: 0 })
+
+            .call(() => {
+                quoteText.innerHTML = text;
+                select(".form")?.classList.remove("hidden");
+            })
+
+            .to(".slide-in > *", { yPercent: 0, stagger: 0.1, duration: 0.5, opacity: 1, ease: "Back.easeOut" })
+            .to(quote, { opacity: 1 }, "<")
+
+            .call(() => PageSetup.assignBtn())
+    }
+}
+
+class PageSetup {
+    constructor(params = {}) {
+        Object.assign(this, params);
+        this.init();
+    }
+
+    init() {
+        const device = Methods.checkDeviceType();
+        const tl = gsap.timeline();
+
+        //Assign button that changes the form
+        PageSetup.assignBtn();
+
+        tl
+            .set(".form-img img", { opacity: 0 })
+            .set(".form-img", { opacity: 0 })
+            .set(".slide-in > *", { yPercent: 100, opacity: 0 })
+            .set(".quote", { opacity: 0 })
+            
+            .to("loader", { display: "none", delay: 1 })
+            .to(".slide-in > *", { yPercent: 0, opacity: 1, stagger: 0.1, ease: "Back.easeOut" })
+            .to(".form-img", { opacity: 1 }, "<")
+            .to(".quote", { opacity: 1 }, "<")
+            .call(() => {
+                if (device != "mobile") {
+                    //Commence the slider animation
+                    const methods = new Methods({ holder: select(".form-img"), max: 7 });
+                    methods.initializeSlider();
+                }
+            })
+    }
+
+    static assignBtn() {
+        select(".form button.arrow").addEventListener("click", () => new Animations({ type: 'slide' }));
+    }
+}
+
+new PageSetup();
